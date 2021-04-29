18cbf0530ab1af81f8e37f691842e8ba56bbd454
minhee <jwmh15@gmail.com>
2021-04-29 18:46:31 +0900

    Create

--- src/main/java/com/oob/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oob.repository;
-
-import com.oob.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
