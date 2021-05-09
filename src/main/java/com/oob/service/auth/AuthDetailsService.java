d2196dbeb8191ff0eaf3a015c1b49881fae47d55
minhee <jwmh15@gmail.com>
2021-05-09 22:39:36 +0900

    [Create] AuthDetailsService

---
+++ src/main/java/com/oob/service/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.oob.service.auth;
+
+import com.oob.domain.repository.UserRepository;
+import com.oob.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .map(AuthDetails::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}
