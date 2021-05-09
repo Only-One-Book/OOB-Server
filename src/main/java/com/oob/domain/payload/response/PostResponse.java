3f7d42b5095eeb88fd96f9e5fb6126ff62b6faf4
minhee <jwmh15@gmail.com>
2021-05-09 23:24:15 +0900

    [Add] Annotation

--- src/main/java/com/oob/domain/payload/response/PostResponse.java
+++ src/main/java/com/oob/domain/payload/response/PostResponse.java
@@ -1,7 +1,12 @@
 package com.oob.domain.payload.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Builder
 public class PostResponse {
     private String title;
     private String content;
